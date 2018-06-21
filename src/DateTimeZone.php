1cd7b648ba1cfc333c6d681ed6c5aca7b420e0bf
Reüel van der Steege <reuel@pronamic.nl>
2018-06-21 17:01:33 +0200

    Use Etc/GMT with offset timezone as fallback instead of GMT.

--- src/DateTimeZone.php
+++ src/DateTimeZone.php
@@ -31,25 +31,53 @@ class DateTimeZone extends \DateTimeZone {
 	public static function get_default() {
 		$timezone_string = get_option( 'timezone_string' );
 
+		if ( ! empty( $timezone_string ) ) {
+			return new DateTimeZone( $timezone_string );
+		}
+
+		$gmt_offset = get_option( 'gmt_offset' );
+		$hours      = (int) $gmt_offset;
+		$minutes    = abs( ( $gmt_offset - (int) $gmt_offset ) * 60 );
+		$offset     = sprintf( '%+03d:%02d', $hours, $minutes );
+
 		/**
 		 * Offset values as timezone parameter are supported since PHP 5.5.10,
 		 * use 'GMT' timezone string instead as default.
 		 *
 		 * @link http://php.net/manual/en/datetimezone.construct.php
 		 */
-		if ( empty( $timezone_string ) && version_compare( PHP_VERSION, '5.5.10', '<' ) ) {
-			$timezone_string = 'GMT';
+		if ( version_compare( PHP_VERSION, '5.5.10', '<' ) ) {
+			$offset = self::transform_offset_to_timezone( $gmt_offset );
 		}
-
-		if ( ! empty( $timezone_string ) ) {
-			return new DateTimeZone( $timezone_string );
-		}
-
-		$offset  = get_option( 'gmt_offset' );
-		$hours   = (int) $offset;
-		$minutes = abs( ( $offset - (int) $offset ) * 60 );
-		$offset  = sprintf( '%+03d:%02d', $hours, $minutes );
 
 		return new DateTimeZone( $offset );
+	}
+
+	/**
+	 * Transform offset to timezone.
+	 *
+	 * @param int|string $gmt_offset GMT offset.
+	 *
+	 * @return string
+	 */
+	private static function transform_offset_to_timezone( $gmt_offset ) {
+		// Ignore minutes in offsets.
+		$dot_position = strrpos( $gmt_offset, '.' );
+
+		if ( false !== $dot_position ) {
+			$gmt_offset = substr( $gmt_offset, 0, $dot_position );
+		}
+
+		// Set timezone.
+		if ( empty( $gmt_offset ) ) {
+			return 'GMT';
+		}
+
+		if ( $gmt_offset < 0 ) {
+			// Need to reverse +/- signs (http://php.net/manual/en/timezones.others.php).
+			return 'Etc/GMT+' . substr( $gmt_offset, 1 );
+		}
+
+		return 'Etc/GMT-' . $gmt_offset;
 	}
 }
